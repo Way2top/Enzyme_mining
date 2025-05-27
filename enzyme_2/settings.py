2fdabbc71d24102b93c3bd58bfa57b26cf74354d
Way2top <3066689820@qq.com>
2025-05-27 10:48:41 +0800

    把数据库的连接密码修改成服务器的

--- enzyme_2/settings.py
+++ enzyme_2/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "enzyme_mining",
         "USER": "root",
-        "PASSWORD": "157867",
+        "PASSWORD": "root123",
 
         "HOST": "127.0.0.1",
         "PORT": "3306",
