c9aa744b984d478d29526f176678f090eb5c7a62
Way2top <3066689820@qq.com>
2025-05-28 14:01:39 +0800

    在settings.py中添加数据库配置

--- enzyme_2/settings.py
+++ enzyme_2/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
 
         "HOST": "127.0.0.1",
         "PORT": "3306",
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', NAMES 'utf8mb4'"
+        }
     }
 }
 
